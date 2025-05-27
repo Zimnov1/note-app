04eddab78895e7e61d4ebf895c563beaef1b5e0e
Zimnov1 <oleksandr.zimnov.shi.2023@lpnu.ua>
2025-05-27 23:40:31 +0300

    Lab_2

--- js/notes.js
+++ js/notes.js
@@ -1,14 +1,14 @@
-const notesContainer = document.getElementById("notesList");
-let notes = JSON.parse(localStorage.getItem("notes")) || [];
+const notesContainer = document.getElementById('notesList');
+const notes = JSON.parse(localStorage.getItem('notes')) || [];
 
 function renderNotes() {
-    notesContainer.innerHTML = "";
+    notesContainer.innerHTML = '';
     if (notes.length === 0) {
         notesContainer.innerHTML = "<p>You don't have any notes.</p>";
     } else {
         notes.forEach((note, index) => {
-            const div = document.createElement("div");
-            div.className = "note";
+            const div = document.createElement('div');
+            div.className = 'note';
             div.innerHTML = `
                 <p>${note.text}</p>
                 <small><strong>Tags:</strong> ${note.tags}</small><br>
@@ -18,18 +18,6 @@ function renderNotes() {
             `;
             notesContainer.appendChild(div);
         });
-    }
-}
-
-function editNote(id) {
-    window.location.href = `note-edit.html?id=${id}`;
-}
-
-function deleteNote(id) {
-    if (confirm("Are you sure you want to delete this note?")) {
-        notes.splice(id, 1);
-        localStorage.setItem("notes", JSON.stringify(notes));
-        renderNotes();
     }
 }
 
